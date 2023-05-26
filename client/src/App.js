fbba96c02a314c4d7caa46a5fa7e6fab93124db0
Kangseonghyeok <kp1525@naver.com>
2023-05-26 20:05:21 +0900

    Commit Message

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,15 @@ const { Title } = Typography;
 function App() {
   return (
     <div>
+       <nav>
+        <div class="inner">
+          <div class="nav-container">
+           <h1  h1 class="nav-title">
+          <a href="http://127.0.0.1:5500/famous-dist-main/front/main.html"><i class="fa fa-arrow-left"></i> 돌아가기</a>
+            </h1>
+            </div>
+        </div>
+      </nav>
       <div style={{ display: 'flex', justifyContent: 'center', marginTop: '2rem' }}>
         <Title level={2} >하르방 챗봇&nbsp;<Icon type="robot" /></Title>
       </div>

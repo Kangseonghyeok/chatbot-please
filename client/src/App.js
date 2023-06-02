90502169204eb8ce168f262e86f7c072715ece84
Kangseonghyeok <kp1525@naver.com>
2023-06-02 17:03:50 +0900

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,27 +5,15 @@ const { Title } = Typography;
 
 function App() {
   return (
-    <div>
-       <nav>
-        <div class="inner">
-          <div class="nav-container">
-           <h1  h1 class="nav-title">
-          <a href="http://127.0.0.1:5500/famous-dist-main/front/main.html"><i class="fa fa-arrow-left"></i> 돌아가기</a>
-            </h1>
-            </div>
-        </div>
-      </nav>
+    <div style={{ backgroundImage: 'url("https://www.bing.com/images/blob?bcid=S866EchhAKYF6Q")', backgroundSize: 'cover', backgroundPosition: 'center', backgroundColor: '#F8F8F8', minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <div style={{ display: 'flex', justifyContent: 'center', marginTop: '2rem' }}>
-        <Title level={2} >하르방 챗봇&nbsp;<Icon type="robot" /></Title>
+        <Title level={2} style={{ color: '#FFFFFF', fontWeight: 'bold', marginBottom: 0, textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)' }}>하르방 챗봇&nbsp;<Icon type="robot" style={{ color: '#7fffd4', fontSize: '2rem' }} /></Title>
       </div>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-       
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '2rem' }}>
         <Chatbot />
-
-
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
